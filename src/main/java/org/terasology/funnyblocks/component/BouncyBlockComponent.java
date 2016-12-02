658d0b186f32c3a9d6c95123350fbee74b1b32ec
Tako ZYGD <zygdtako@outlook.com>
2016-12-02 15:32:44 +0800

    Added Bouncy Block
    - Added Bouncy Block

--- src/main/java/org/terasology/funnyblocks/component/BouncyBlockComponent.java
+++ src/main/java/org/terasology/funnyblocks/component/BouncyBlockComponent.java
@@ -3,4 +3,5 @@ package org.terasology.funnyblocks.component;
 import org.terasology.entitySystem.Component;
 
 public class BouncyBlockComponent implements Component {
-	}
+	public float force = 20;
+}
--- src/main/java/org/terasology/funnyblocks/system/BouncySystem.java
+++ src/main/java/org/terasology/funnyblocks/system/BouncySystem.java
@@ -4,8 +4,6 @@ import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.entitySystem.systems.UpdateSubscriberSystem;
 
-import java.io.Console;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.engine.Time;
@@ -13,15 +11,14 @@ import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.logic.characters.CharacterImpulseEvent;
+import org.terasology.logic.characters.CharacterMoveInputEvent;
 import org.terasology.logic.characters.CharacterMovementComponent;
-import org.terasology.logic.characters.events.OnEnterBlockEvent;
 import org.terasology.logic.location.LocationComponent;
 import org.terasology.math.geom.Vector3f;
 import org.terasology.math.geom.Vector3i;
 import org.terasology.registry.In;
 import org.terasology.world.BlockEntityRegistry;
 import org.terasology.world.WorldProvider;
-import org.terasology.world.block.Block;
 import org.terasology.entitySystem.systems.RegisterMode;
 
 import org.terasology.funnyblocks.component.BouncyBlockComponent;
@@ -51,44 +48,20 @@ public class BouncySystem extends BaseComponentSystem implements UpdateSubscribe
 	private static final Logger logger = LoggerFactory.getLogger(BouncySystem.class);
 
 	@ReceiveEvent
-	public void onEnterBlock(OnEnterBlockEvent event, EntityRef player) 
+    public void onCharacterMovement(CharacterMoveInputEvent event, EntityRef player) 
 	{
-		if (!isAtHeadLevel(event.getCharacterRelativePosition(), player)) 
+		Vector3f playerPosition = new Vector3f(player.getComponent(LocationComponent.class).getWorldPosition());
+		for (EntityRef blockEntity : entityManager.getEntitiesWith(BouncyBlockComponent.class,LocationComponent.class)) 
 		{
-			return;
-		}
-		
-		Block block = event.getNewBlock();
-		if(isBouncyBlock(block)){
-			Vector3f playerPosition = new Vector3f(player.getComponent(LocationComponent.class).getWorldPosition());
-			if (worldProvider.getBlock(playerPosition.subY(1)).getEntity().getComponent(BouncyBlockComponent.class) != null)
+			Vector3f blockPos = blockEntity.getComponent(LocationComponent.class).getWorldPosition();
+			if(blockPos!=null && playerPosition!=null)
 			{
-				Vector3f impulse = playerPosition.normalize();
-				impulse.set(impulse.subY(1));
-				player.send(new CharacterImpulseEvent(impulse));
-			}			
-		}
-	}
-	
-	/**
-	 * Check is the entity at head level
-	 * @param relativePosition the specified position 
-	 * @param entity                the enity
-	 * @return                          true if the enitity is at head level of the specified position 
-	 */
-	private boolean isAtHeadLevel(Vector3i relativePosition, EntityRef entity) 
-	{
-		CharacterMovementComponent characterMovementComponent = entity.getComponent(CharacterMovementComponent.class);
-		return (int) Math.ceil(characterMovementComponent.height) - 1 == relativePosition.y;
-	}
-	
-	/**
-	 * Check the block type
-	 * @param block  the block
-	 * @return           true if the block has BouncyBlockComponent
-	 */
-	private boolean isBouncyBlock(Block block)
-	{
-        return block.getEntity().hasComponent(BouncyBlockComponent.class);
-	}
+				if(Math.round(playerPosition.x) == blockPos.x && Math.round(playerPosition.z) == blockPos.z && Math.round(playerPosition.y - 1) == blockPos.y) 
+				{
+						Vector3f impulse = new Vector3f(0,blockEntity.getComponent(BouncyBlockComponent.class).force,0);
+						player.send(new CharacterImpulseEvent(impulse));
+				}
+            }
+        }
+	}	
 }
