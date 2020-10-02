4daee18033f96aee8dbb4df089eca44405a37ae5
Michael Pollind <mpollind@gmail.com>
2020-10-02 12:58:06 -0700

    resolve problem with changes from PR: https://github.com/MovingBlocks/Terasology/pull/4177

--- src/main/java/org/terasology/speedbooster/SpeedBoosterSystem.java
+++ src/main/java/org/terasology/speedbooster/SpeedBoosterSystem.java
@@ -16,6 +16,7 @@
 
 package org.terasology.speedbooster;
 
+import org.joml.Vector3f;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.entitySystem.entity.EntityManager;
@@ -29,7 +30,7 @@ import org.terasology.logic.characters.CharacterImpulseEvent;
 import org.terasology.logic.characters.CharacterMovementComponent;
 import org.terasology.logic.characters.events.OnEnterBlockEvent;
 import org.terasology.logic.location.LocationComponent;
-import org.terasology.math.geom.Vector3f;
+import org.terasology.math.JomlUtil;
 import org.terasology.registry.In;
 import org.terasology.speedbooster.component.SpeedBoosterComponent;
 import org.terasology.world.BlockEntityRegistry;
@@ -50,31 +51,30 @@ public class SpeedBoosterSystem extends BaseComponentSystem implements UpdateSub
     private WorldProvider worldProvider;
 
     @Override
-    public void update(float delta) {}
+    public void update(float delta) {
+    }
 
     @ReceiveEvent(components = {LocationComponent.class, CharacterMovementComponent.class})
     public void onEnterBlock(OnEnterBlockEvent event, EntityRef entity) {
 
         LocationComponent loc = entity.getComponent(LocationComponent.class); // get player LocationComponent
-        Vector3f pos = loc.getWorldPosition(); // get player position
-        pos.setY(pos.getY() -1); // get position below player
+        Vector3f pos = loc.getWorldPosition(new Vector3f()); // get player position
+        pos.y = pos.y() - 1; // get position below player
         Block block = worldProvider.getBlock(pos); // get block at that position
-        CharacterMovementComponent cmc = entity.getComponent( CharacterMovementComponent.class );
-        SpeedBoosterComponent sbc = block.getEntity().getComponent( SpeedBoosterComponent.class );
-        if ( sbc != null ) {
+        CharacterMovementComponent cmc = entity.getComponent(CharacterMovementComponent.class);
+        SpeedBoosterComponent sbc = block.getEntity().getComponent(SpeedBoosterComponent.class);
+        if (sbc != null) {
             // this will increase the speed of player
             cmc.speedMultiplier = sbc.speedMultiplier;
             entity.saveComponent(cmc);
-            Vector3f imp = cmc.getVelocity().normalize().scale(64).setY(6);
-            entity.send(new CharacterImpulseEvent(imp));
+            Vector3f imp = new Vector3f(cmc.getVelocity()).normalize().mul(64);
+            imp.y = 6;
+            entity.send(new CharacterImpulseEvent(JomlUtil.from(imp)));
         } else {
             // this will return speed of player to normal
             cmc.speedMultiplier = 1f;
             entity.saveComponent(cmc);
         }
     }
-
-
-
 }
 
