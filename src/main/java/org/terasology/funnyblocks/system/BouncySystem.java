8f6c9286afebedbb7a2579310b27d253ba9e4431
Michael Pollind <mpollind@gmail.com>
2020-12-06 09:56:52 -0800

    feat(JOML): migrate charactermovment PR: https://github.com/MovingBlocks/Terasology/pull/4303

--- src/main/java/org/terasology/funnyblocks/system/BouncySystem.java
+++ src/main/java/org/terasology/funnyblocks/system/BouncySystem.java
@@ -15,6 +15,7 @@
  */
 package org.terasology.funnyblocks.system;
 
+import org.joml.Vector3f;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterSystem;
 
@@ -27,7 +28,6 @@ import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.logic.characters.CharacterImpulseEvent;
 import org.terasology.logic.characters.CharacterMoveInputEvent;
 import org.terasology.logic.location.LocationComponent;
-import org.terasology.math.geom.Vector3f;
 import org.terasology.registry.In;
 import org.terasology.world.BlockEntityRegistry;
 import org.terasology.world.WorldProvider;
@@ -56,10 +56,10 @@ public class BouncySystem extends BaseComponentSystem {
         if (player.getComponent(LocationComponent.class) == null || player.getComponent(LocationComponent.class).getWorldPosition() == null) {
             return;
         }
-        
-        Vector3f playerPosition = new Vector3f(player.getComponent(LocationComponent.class).getWorldPosition());
+
+        Vector3f playerPosition = player.getComponent(LocationComponent.class).getWorldPosition(new Vector3f());
         for (EntityRef blockEntity : entityManager.getEntitiesWith(BouncyBlockComponent.class, LocationComponent.class)) {
-            Vector3f blockPos = blockEntity.getComponent(LocationComponent.class).getWorldPosition();
+            Vector3f blockPos = blockEntity.getComponent(LocationComponent.class).getWorldPosition(new Vector3f());
             if (blockPos != null && playerPosition != null) {
                 if (Math.round(playerPosition.x) == blockPos.x && Math.round(playerPosition.z) == blockPos.z
                         && Math.round(playerPosition.y - 1) == blockPos.y) {
